28f238fe0c18af34f00daed5552f07ceb803b3e1
elusiveellie <elena@realemail.net>
2024-01-03 18:34:31 -0500

    Added new adjectives and nouns

--- backend/generator.js
+++ backend/generator.js
@@ -1,5 +1,5 @@
-const adjectives = ['Yellow', 'Agressive', 'Cheerful', 'Charmful', "Tiny"];
-const nouns = ['Rock', 'Beaver', 'Door', "Lamp", "Ink"];
+const adjectives = ['Yellow', 'Aggressive', 'Cheerful', 'Charmful', "Tiny", "Bothersome", "Gwumpy", "Feathery"];
+const nouns = ['Rock', 'Beaver', 'Door', "Lamp", "Ink", "Box", "Feather", "Waltz"];
 
 const generator = () => {
   return adjectives[Math.floor(Math.random() * adjectives.length)] + " " + nouns[Math.floor(Math.random() * adjectives.length)];
